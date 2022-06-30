7f63e4af3f7f5f725a5c527270cf1ddbdfed3d48
tibrook <guillouet.alexandre35@gmail.com>
2022-06-30 19:55:03 +0200

    réorganisation
    réorganisation

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-//const { createTypeReferenceDirectiveResolutionCache } = require("typescript");
-const User = require("./models/user");
-//const Thing = require('./models/thing');
-const app = express();
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/user");
+
 mongoose
   .connect(
     "mongodb+srv://guillou:0508@ocp6.euv7huo.mongodb.net/?retryWrites=true&w=majority",
@@ -11,6 +10,8 @@ mongoose
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
 
 app.use(express.json());
 
@@ -26,28 +27,7 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.post("/api/auth/signup", (req, res, next) => {
-  const user = new User({
-    ...req.body, // L'opérateur spread est utilisé pour faire une copie de toutes les infos de req.body
-  });
 
-  User.findOne({ email: req.body.email }).then((userfound) => {
-    console.log(user);
-    if (userfound !== null) {
-      console.log("L'utilisateur est déjà enregistré");
-      return res.status(201).json({ message: "User déjà enregistré !" });
-    } else {
-      user
-        .save()
-        .then(() => res.status(201).json({ message: "User enregistré !" }))
-        .catch((error) => res.status(400).json({ error }));
-      console.log("Enregistrement de l'utilisateur");
-      return;
-    }
-  });
-});
+app.use(bodyParser.json());
+app.use("/api/auth", userRoutes);
 module.exports = app;
-/* if (user !== null) {
-  alert("Cette adresse mail est déjà enregistrée");
-  return "Cette adresse mail est déjà enregistrée";
-} */
